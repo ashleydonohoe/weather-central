9d92da274e25e8decf12691e06e1cfdfc64b3b47
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-08-17 19:07:50 -0400

    connecting form

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+var PropTypes = require("prop-types");
 
 class Form extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        locationName: ""
+      };
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    var locationValue = event.target.value;
+
+    this.setState(function() {
+      return {
+        locationName: locationValue
+      }
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.locationName);
+  }
+
   render() {
     return (
-      <form className="weather-form">
-        <input placeholder="city, state" name="city"/>
-        <button className="weather-form-button">Get Weather</button>
+      <form className="weather-form" onSubmit={this.handleSubmit} value={this.state.locationName}>
+        <input placeholder="city, state" name="city" onChange={this.handleChange}/>
+        <button type="submit" className="weather-form-button">Get Weather</button>
       </form>
     );
   }
+}
+
+Form.propTypes = {
+  onSubmit: PropTypes.func.isRequired
 }
 
 export default Form;
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import Form from "./Form";
 
 class Home extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        locationName: "",
+        locationData: null
+      };
+
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(locationName) {
+    var locationData;
+
+    this.setState(function() {
+      var newState = {};
+      newState.locationName = locationName;
+
+      // TODO: Write API call to get the data for the location's weather
+      newState.locationData = locationData;
+    });
+  }
+
   render() {
     return (
       <div className="home">
-        <h2>Home component</h2>
+        <h1>Enter a City and State</h1>
+        <Form onSubmit={this.handleSubmit}/>
       </div>
     );
   }
