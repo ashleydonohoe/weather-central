535ba51496fc3e1ff38ccc537fcf5009fc04ca2d
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-08-23 22:57:12 -0400

    working on api call for the nav

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import Form from "./Form";
+var api = require("../utils/api");
 
 class Nav extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        locationName: "",
+        locationData: null
+      };
+
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(locationName) {
+    api.fetchCurrentWeather(locationName).then(function(forecast) {
+      console.log(forecast);
+      this.setState(function() {
+        return {
+          locationName: locationName,
+          locationData: forecast
+        }
+      });
+    }.bind(this));
+  }
   render() {
     return (
       <header className="nav">
